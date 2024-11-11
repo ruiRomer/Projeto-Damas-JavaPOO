0ca7e87cd9afc97df1ecee67385716f6c7414e1a
Rui Romer <ruiromer7a@gmail.com>
2024-11-11 15:53:42 -0300

    Chequemategit status

--- src/app/UI.java
+++ src/app/UI.java
@@ -60,9 +60,14 @@ public class UI {
 		printCapturedPìeces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
 			System.out.println("CHECK!");
+			}
+		}else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
