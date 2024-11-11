f5355d06a9bd9d58ee3c99cea7733e0c1cbc90c7
Rui Romer <ruiromer7a@gmail.com>
2024-11-11 19:45:44 -0300

    Arrumando tratamento de promoção

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -127,7 +126,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no peace to be promoted");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
@@ -264,7 +263,7 @@ public class ChessMatch {
 	}
 	
 	private Color opponent(Color color) {
-		return (color == Color.WHITE) ? color.BLACK : color.WHITE;
+		return (color == Color.WHITE) ? Color.BLACK : Color.WHITE; 
 	}
 	
 	private ChessPiece king(Color color) {
